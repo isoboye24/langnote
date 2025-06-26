22dc5486eb8083deaeed4dfc9d26418eefb258a0
isoboye24 <isobarguy4real@gmail.com>
2025-06-26 12:59:03 +0200

    I updated the design of the list of groups

--- components/ui/shared/single-word-group.tsx
+++ components/ui/shared/single-word-group.tsx
@@ -20,22 +20,23 @@ const SingleWordGroup = ({
 
   return (
     <>
-      <div
-        className="flex flex-col-[12px_1fr] w-full h-full rounded shadow-md overflow-hidden"
-        // style={{ minHeight: '100px' }}
-      >
-        <div className="w-2 h-full" style={{ backgroundColor: color }} />
-        <div className="flex-1 flex flex-col">
-          <div className="bg-gray-800 dark:bg-amber-100 text-gray-200 dark:text-gray-800 text-sm md:text-base font-semibold p-2 truncate">
+      <div className="grid grid-cols-[12px_1fr] w-full h-full rounded shadow-md ">
+        <div className="w-full h-full" style={{ backgroundColor: color }} />
+        <div className="grid grid-cols-[1fr_80px] bg-gray-200 dark:bg-gray-800">
+          <div className="  text-sm md:text-base font-semibold p-2 truncate">
             {groupName}
           </div>
-          <div className="flex justify-between items-center px-2 py-1 bg-amber-300 dark:bg-amber-200">
-            <Button className="bg-transparent p-1">
-              <Pen className="text-gray-900 w-4 h-4" />
-            </Button>
-            <Button className="bg-transparent p-1">
-              <Trash2 className="text-red-500 w-4 h-4" />
-            </Button>
+          <div className="justify-items-end flex ">
+            <div className="flex-1">
+              <Button className="bg-transparent p-1 hover:bg-gray-400 dark:hover:bg-gray-600">
+                <Pen className="text-gray-900 dark:text-gray-300 w-4 h-4" />
+              </Button>
+            </div>
+            <div className="flex-1">
+              <Button className="bg-transparent p-1 hover:bg-gray-400 dark:hover:bg-gray-600">
+                <Trash2 className="text-red-500 w-4 h-4" />
+              </Button>
+            </div>
           </div>
         </div>
       </div>
